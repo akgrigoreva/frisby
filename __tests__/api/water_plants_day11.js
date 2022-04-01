2a8c402424341e510ad75a31685ff601a7155682
akgrigoreva <101564773+akgrigoreva@users.noreply.github.com>
2022-04-01 10:13:44 +0300

    Update water_plants_day11.js

--- __tests__/api/water_plants_day11.js
+++ __tests__/api/water_plants_day11.js
@@ -7,6 +7,21 @@ describe("Should i water the plants?", () => {
         .expect("jsonTypes", {"level": Joi.number()})
         .then(result => {
   console.log(result.json);
-  });
-});
     });
+  });
+    test.todo("water the plants if necessary"), () => {
+if (result => {
+          const body = result.json;
+          const found = body.results.find(result => result.level > 0.60); {
+  console.log(result.json);
+  console.log("plants are wet");
+  } if (result => {
+          const body = result.json;
+          const found = body.results.find(result => result.level < 0.60); {
+  frisby.post(water-ttl.herokuapp.com/water, {duration:10}).expect('status', 200);
+    console.log("i watered the plants!")
+        };
+      });
+    });
+  };
+});
